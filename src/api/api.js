c71f22c4abd7125572c66a3f904ef67450a342c1
Rtif173 <13.meilleur@gmail.com>
2023-01-24 21:53:24 +0200

    load cards to server api

--- src/api/api.js
+++ src/api/api.js
@@ -1,44 +1,74 @@
 import CryptoJS from 'crypto-js';
 
 const backendURL = "http://memorymate.pythonanywhere.com/api/v1/"
-export class API{
-  async isEmailRegistered(email){
-    const resp = await fetch(backendURL+"is_email_registered?email="+email);
+export class API {
+  session = localStorage.getItem("session") 
+
+  async isEmailRegistered(email) {
+    const resp = await fetch(backendURL + "is_email_registered?email=" + email);
     const status = resp.status
     console.log(status);
-    if(status == 200){
+    if (status == 200) {
       const t = await resp.json()
       return [t[0], null]
     }
-    else{
+    else {
       const t = await resp.json()
       return [t.error, status]
     }
   }
-  async register(username, email, password){
+  async register(username, email, password) {
     const pwd = CryptoJS.SHA256(password).toString();
-    const resp = await fetch(backendURL+`register?username=${username}&email=${email}&pwd=${pwd}`);
+    const resp = await fetch(backendURL + `register?username=${username}&email=${email}&pwd=${pwd}`);
     const status = resp.status;
-    if(status == 200){
+    if (status == 200) {
       const t = await resp.text()
       return [t, null]
     }
-    else{
+    else {
       const t = await resp.json()
       return [t.error, status]
     }
   }
-  async login(email, password){
+  async login(email, password) {
     const pwd = CryptoJS.SHA256(password).toString();
-    const resp = await fetch(backendURL+`login?email=${email}&pwd=${pwd}`);
+    const resp = await fetch(backendURL + `login?email=${email}&pwd=${pwd}`);
     const status = resp.status;
-    if(status == 200){
+    if (status == 200) {
       const t = await resp.json()
       return [t.session, null]
     }
-    else{
+    else {
       const t = await resp.json()
       return [t.error, status]
+    }
+  }
+  async sendCardsToServer(name, cards, media) {
+    const jsonData = { "cards": cards, "media": media };
+    const jsonBlob = new Blob([JSON.stringify(jsonData)], { type: 'application/json' });
+    const formData = new FormData();
+    formData.append('file', jsonBlob, `${name}.json`);
+
+    fetch(backendURL + `upload_cardset?session=${this.session}`, {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => response.text())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }
+  async loadCardsFromServer(username, cardsetname){
+    const resp = await fetch(backendURL+username+'/'+cardsetname);
+    const status = resp.status;
+    if (status == 200) {
+      json = await resp.json();
+      if("cards" in json && "media" in json){
+        return json, null
+      } else {
+        return "cardset has invalid properties", 500
+      }
+    } else {
+      return (await resp.json()).error, status
     }
   }
 }
