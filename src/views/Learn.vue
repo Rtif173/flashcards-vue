91b60d3009f983b55cf614b01ae8ae1aa3b96953
Rtif173 <13.meilleur@gmail.com>
2023-02-03 23:17:50 +0200

    custom progressbar

--- src/views/Learn.vue
+++ src/views/Learn.vue
@@ -45,8 +45,10 @@ function setDefaultStoreLearn() {
           <div class="progress-counter">{{ storeLearn.good.length }}</div>
         </div>
       </div>
-      <progress class="learn-screen-item animated" :value="storeLearn.currentCard" :max="storeLearn.line.length"
-        :style="'--value: ' + storeLearn.currentCard / storeLearn.line.length * 100 + '%'"></progress>
+      <div class="progress">
+        <div class="bar" :style="'width: ' + storeLearn.currentCard / storeLearn.line.length * 100 + '%'">
+        </div>
+      </div>
       <Card />
     </div>
     <div v-if="Object.keys(store.cards).length == 0" class="no-cards">
@@ -77,8 +79,25 @@ function setDefaultStoreLearn() {
   margin-left: var(--big-padding);
 }
 
-progress.learn-screen-item {
+/* progress.learn-screen-item {
   width: calc(100% - 2*var(--big-padding));
+} */
+
+.progress {
+  width: 100%;
+  height: 0.5rem;
+  margin-bottom: calc(var(--spacing) * 0.5);
+  overflow: hidden;
+  border: 0;
+  border-radius: var(--border-radius);
+  background-color: var(--progress-background-color);
+  color: var(--progress-color);
+}
+
+.progress .bar {
+  height: 100%;
+  background-color: var(--progress-color);
+  transition: width .5s ease-in;
 }
 
 .progress-info-wrapper {
